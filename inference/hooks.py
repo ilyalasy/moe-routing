364a632893494251b165b6d054bbbf7f5fa2f848
Ilya Lasy <ilyalas@gmail.com>
2024-03-14 14:58:38 +0100

    fix hook to consider expert capacity

--- inference/hooks.py
+++ inference/hooks.py
@@ -11,22 +11,19 @@ def set_router_hook(
     model: torch.nn.Module,
 ) -> Tuple[Dict, List[RemovableHandle]]:
     def create_hook(save_dict: Dict, layer_name: str) -> Callable:
-        def hook(module, args, kwargs):  #
-            inputs = kwargs["inputs"]
-            # copied from https://github.com/hpcaitech/ColossalAI/blob/5d380a1a215204d827604c4797be12aad001424a/colossalai/moe/routers.py#L225
-            probs = F.softmax(inputs, dim=-1)
-            num_experts = probs.size(-1)
-
-            top1_idx = torch.argmax(probs, dim=-1)
-            mask1 = F.one_hot(top1_idx, num_classes=num_experts).to(
-                torch.int32
-            )
-            logits_except1 = probs.masked_fill(mask1.bool(), float("-inf"))
-            top2_idx = torch.argmax(logits_except1, dim=-1)
-            chosen_experts = (
-                torch.stack([top1_idx, top2_idx], dim=-1).detach().cpu()
-            )
-            save_dict[layer_name].append(chosen_experts)
+        def hook(module: Top2Router, args, kwargs, output):  #
+            # see https://github.com/hpcaitech/ColossalAI/blob/5d380a1a215204d827604c4797be12aad001424a/colossalai/moe/layers.py#L172
+            experts_mask = output[1]
+            experts_per_token = []
+            for tok_experts in experts_mask:
+                full_experts = torch.full(
+                    (module.k_value,), -1
+                )  # keep shape even if token is overflowed
+                selected_experts = tok_experts.nonzero(as_tuple=True)[0]
+                full_experts[: len(selected_experts)] = selected_experts
+                experts_per_token.append(full_experts)
+            experts_per_token = torch.stack(experts_per_token).detach().cpu()
+            save_dict[layer_name].append(experts_per_token)
 
         return hook
 
@@ -34,7 +31,7 @@ def set_router_hook(
     activated_experts = defaultdict(list)
     for name, module in model.named_modules():
         if isinstance(module, Top2Router):
-            handle = module.register_forward_pre_hook(
+            handle = module.register_forward_hook(
                 create_hook(activated_experts, name), with_kwargs=True
             )
             hooks.append(handle)
